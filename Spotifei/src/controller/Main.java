4fd52a0e42b8f65fa3d216881d97daf238ef8c00
Henrique Bassan Rebechi <hbrzaodocs@gmail.com>
2025-05-18 04:26:00 -0300

    feat: ajustes no .gitignore e inclusão de controller e dao

---
+++ Spotifei/src/controller/Main.java
@@ -0,0 +1,15 @@
+package controller;
+
+import dao.Conexao;
+import java.sql.Connection;
+
+public class Main {
+    public static void main(String[] args) {
+        Connection conexao = Conexao.conectar();
+        if (conexao != null) {
+            System.out.println("Conexão bem-sucedida!");
+        } else {
+            System.out.println("Falha na conexão.");
+        }
+    }
+}
