21b5103ad8ab7c93195d9b148e535538b65ceb4d
Henrique Bassan Rebechi <hbrzaodocs@gmail.com>
2025-05-18 03:53:04 -0300

    feat(model): criação da classe Musica com atributos e relação com Artista

---
+++ Spotifei/src/model/Musica.java
@@ -0,0 +1,66 @@
+package model;
+
+public class Musica {
+    private int id;
+    private String titulo;
+    private int duracao; // em segundos
+    private String genero;
+    private int idArtista;
+
+    public Musica() {}
+
+    public Musica(int id, String titulo, int duracao, String genero, int idArtista) {
+        this.id = id;
+        this.titulo = titulo;
+        this.duracao = duracao;
+        this.genero = genero;
+        this.idArtista = idArtista;
+    }
+
+    // Getters e Setters
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public int getDuracao() {
+        return duracao;
+    }
+
+    public void setDuracao(int duracao) {
+        this.duracao = duracao;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public int getIdArtista() {
+        return idArtista;
+    }
+
+    public void setIdArtista(int idArtista) {
+        this.idArtista = idArtista;
+    }
+
+    @Override
+    public String toString() {
+        return "Música: " + titulo + " | Gênero: " + genero + " | Duração: " + duracao + "s";
+    }
+}
